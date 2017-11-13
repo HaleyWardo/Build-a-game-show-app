f9ddccc963c659f4f993e350c75049dd0b895bf1
Haley Ward <hayevans18@gmail.com>
2017-11-13 17:01:34 -0600

    Added checkLetter function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-const qwertyDiv = document.getElementById('qwerty');    
-const phraseDiv = document.getElementById('phrase');
-const startButton = document.getElementsByClassName('btn__reset')[0];
+/////////////
+//CONSTANTS
+/////////////
 
-let missed = 0;   // number of missed guesses 
+const phraseDiv = document.getElementById('phrase');
+const ul = document.getElementsByTagName('ul');
+const qwertyDiv = document.getElementById('qwerty');   
+const letterButton = qwertyDiv.querySelectorAll('button');
+const startButton = document.getElementsByClassName('btn__reset')[0];
 
 const phrases = [
     'A Chip Off The Old Block',
@@ -12,20 +16,70 @@ const phrases = [
     'Keeping It Real',
 ];
 
-/**
- * Function that takes an array and returns a randomly selected phrase. 
- * Then it splits the phrase into characters and uppercases all letters.
- * @param {*} array 
- */
+
+/////////////
+//VARIABLES
+/////////////
+
+let missed = 0;   // number of missed guesses 
+
+
+/////////////
+//FUNCTIONS
+/////////////
+
+
+//function to get random phrase and split characters
 function getRandomPhraseAsArray(array) {
     const randomPhrase = array[Math.floor(Math.random()*phrases.length)];
     return randomPhrase.toUpperCase().split('');
 }
 
-startButton.addEventListener('click', () => {    //event listener for when button is clicked
-    startButton.style.display = 'none';
+const phraseArray = getRandomPhraseAsArray(phrases);
+
+
+//function to add list item and display random phrase
+function addPhraseToDisplay(array) {   
+    for (let i = 0; i < phraseArray.length; i++) {
+        const listItem = document.createElement('li');
+        ul[0].appendChild(listItem);
+        listItem.textContent = phraseArray[i];
+    
+        if (phraseArray[i] != ' ') {
+            listItem.className = 'letter';
+        } else {
+            listItem.className = 'space';
+        }
+    }
+}
+addPhraseToDisplay(phraseArray);
+
+
+
+function checkLetter(buttonClicked) {
+    const letter = document.getElementsByClassName('letter');
+    for (let i = 0; i < letter.length; i++){
+        if (buttonClicked.textContent === letter[i].textContent) {
+            letter.className = 'show';
+        } else {
+            return null;
+        }
+    }
+}
+
+
+//////////////////
+//EVENT HANDLERS
+/////////////////
+
+//Listener for when start button is clicked
+startButton.addEventListener('click', () => {  
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = "none";
 });
 
-
-
-
+//listener for when user presses a key on keyboard
+qwertyDiv.addEventListener('keypress', (e) => {
+    e.target.className = 'chosen';
+    checkLetter();
+});
